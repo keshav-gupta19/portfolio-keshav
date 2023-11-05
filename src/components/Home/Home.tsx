9b38c9dfb84a9652e7efc5b6d48a709d3f723c2e
Keshav Gupta <keshavkk191103@gmail.com>
2023-11-05 23:51:22 +0530

    keshav-portfolio

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/objetivo1.png";
+import homeLogo from "../../assets/objetivo1.png";
 import Home2 from "./Home2";
 import Type from "./Type";
 
